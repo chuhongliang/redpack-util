dcbdee56bffcf11b496ada6152b3534fe3b4b9fa
chuhongliang <13717807729@163.com>
2020-01-02 20:10:06 +0800

    fix num is not enough bug

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const redPackUtil = require('../lib/redpack-util');
 
 
-let result = redPackUtil.createRandomPacks(10000, 100, 300);
+let result = redPackUtil.createRandomPacks(1000, 10, 300);
 console.log(result);
